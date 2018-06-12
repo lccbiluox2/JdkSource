b16d8c5aa6563f92be091e43cbe99706d2570f7d
lcc <541711153@qq.com>
2018-06-12 15:01:37 +0800

    jdk test

---
+++ src/main/java/com/lcc/jdk/test/outOutMemoryError/HeapOOM.java
@@ -0,0 +1,19 @@
+package com.lcc.jdk.test.outOutMemoryError;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HeapOOM {
+
+    static class OOMObject{
+
+    }
+
+    public  static void main(String[] args){
+        List<OOMObject> list = new ArrayList<OOMObject>();
+
+        while(true){
+            list.add(new OOMObject());
+        }
+    }
+}
