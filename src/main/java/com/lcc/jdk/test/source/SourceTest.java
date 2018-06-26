9649511633e6365e17a53cf71ae9fa1ab439058a
lcc <541711153@qq.com>
2018-06-26 09:54:41 +0800

    jdk test

--- src/main/java/com/lcc/jdk/test/source/SourceTest.java
+++ src/main/java/com/lcc/jdk/test/source/SourceTest.java
@@ -1,11 +1,23 @@
 package com.lcc.jdk.test.source;
 
+
 import java.util.AbstractCollection;
 import java.util.HashMap;
+import java.util.Map;
 
 public class SourceTest {
 
     public static void main(String[] args){
-        HashMap
+        Map<String,Integer> aa= new HashMap<String,Integer>();
+
+        for(int i=0;i<1000;i++){
+            aa.put("aa"+i,i);
+        }
+
+
+        System.out.println(aa.size());
+        System.out.println(aa.get("aa"));
+
+
     }
 }
