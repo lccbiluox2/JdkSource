1c52cf5f24a07628998d87f9bbda1ab0b70075ad
lcc <541711153@qq.com>
2018-06-27 14:05:56 +0800

    jdk test

---
+++ src/main/java/com/antlr/ArrayInitTest.java
@@ -0,0 +1,26 @@
+package com.antlr;
+
+
+import com.antlr.arrayinit.ArrayInitLexer;
+import com.antlr.arrayinit.ArrayInitParser;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+
+public class ArrayInitTest {
+
+    public static void main(String[] args) throws Exception{
+
+        ANTLRInputStream input = new ANTLRInputStream(System.in);
+
+        ArrayInitLexer lexer = new ArrayInitLexer(input);
+
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        ArrayInitParser parser = new ArrayInitParser(tokens);
+
+        ParseTree tree = parser.init();
+
+        System.out.println(tree.toStringTree(parser));
+    }
+}
